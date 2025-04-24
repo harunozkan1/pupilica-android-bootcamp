2fbcb69a26e6dda9171c66520c244d26efbad3a5
harunozkan1 <106629980+harunozkan1@users.noreply.github.com>
2025-04-24 22:16:36 +0300

    Add files via upload

---
+++ Odev1.kt
@@ -0,0 +1,51 @@
+package com.example.kotlindersleri.degiskenler
+
+fun main() {
+    var sehir:String ="Yozgat"
+    var ulke:String ="Turkiye"
+    var telefon:Long =3541234567
+    var postaKodu:Int = 66000
+    var email:String="harun@mynet.com"
+    println("Adres: Yozgat, Merkez, Fatih Mahallesi, Lale Sokak No: 5, Daire: 8\nSehir: $sehir\nUlke: $ulke\nPosta Kodu: $postaKodu\nTelefon: $telefon\nE-posta: $email")
+    var meslek: String = "Manifaturacı"
+    var stokMiktari: Int = 99
+    var musteriAdi: String = "Darth Vader"
+    var bakiye: Double = 1453.75
+    var dogumGunu: String = "04/01/1910"
+    var maas: Double = 26005.50
+    var medeniDurum: String = "Bekar"
+    var urunYorumu: String = "Bir beden buyuk alin"
+    var odemeTarihi: String = "01/05/2025"
+    var odeme: Int = 3
+    var siparisAdeti: Int = 3
+    var arabaModeli: String = "A3"
+    var kitapAdi: String = "Simyaci"
+    var yayinlamaTarihi: String = "07-01-2012"
+    var indirimMiktari: Double = 39.90
+    var odaSayisi: Int = 4
+    var enlem: Double = 36.12345
+    var boylam: Double = 42.67890
+    var urunAdi: String = "Tras Losyonu"
+    var yemekFiyati: Double = 273.25
+    var marka: String = "Louis Vuitton"
+    var muzikAdi: String = "Kenan Dogulu:Shake it up Sekerim"
+    var videoSuresi: Int = 12
+    var urunPuani: Double = 4.7
+    var resimAdi: String = "images.jpeg"
+    var dosyaFormati: String = "pdf"
+    var renk: String = "Yesil"
+    var renkKodu: String = "#54CAAB"
+    var telefonModeli: String = "LG G3"
+    var ekranBoyutu: String = "5 inç"
+    var agirlik: Int = 73
+    var ulusalGun: String = "24 Kasim"
+    var tatilGunu: String = "Pazar"
+    var rezervasyonTarihi: String = "16 Haziran 1950"
+    var sokakAdi: String = "Gul Sokak"
+    var otobusHatti: String = "29D"
+    var kalanDakika: Int = 3
+    var takipKodu: String = "A1232415"
+    var kuponSuresi: Int = 7
+    var kuponKodu: String = "KL4VY3"
+    var faturaAdresi: String = "Zonguldak, Devrek, Kamelya Sk. No: 10"
+}
